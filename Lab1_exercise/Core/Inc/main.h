0327adbe85fe793879db3b5127eb300873a0fb8a
thienhuy2702 <huy.tran2702@hcmut.edu.vn>
2025-09-30 00:54:40 +0700

    update

--- Lab1_exercise/Core/Inc/main.h
+++ Lab1_exercise/Core/Inc/main.h
@@ -57,10 +57,30 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
+#define time_0_Pin GPIO_PIN_4
+#define time_0_GPIO_Port GPIOA
+#define time_1_Pin GPIO_PIN_5
+#define time_1_GPIO_Port GPIOA
+#define time_2_Pin GPIO_PIN_6
+#define time_2_GPIO_Port GPIOA
+#define time_3_Pin GPIO_PIN_7
+#define time_3_GPIO_Port GPIOA
+#define time_4_Pin GPIO_PIN_8
+#define time_4_GPIO_Port GPIOA
+#define time_5_Pin GPIO_PIN_9
+#define time_5_GPIO_Port GPIOA
+#define time_6_Pin GPIO_PIN_10
+#define time_6_GPIO_Port GPIOA
+#define time_7_Pin GPIO_PIN_11
+#define time_7_GPIO_Port GPIOA
+#define time_8_Pin GPIO_PIN_12
+#define time_8_GPIO_Port GPIOA
+#define time_9_Pin GPIO_PIN_13
+#define time_9_GPIO_Port GPIOA
+#define time_10_Pin GPIO_PIN_14
+#define time_10_GPIO_Port GPIOA
+#define time_11_Pin GPIO_PIN_15
+#define time_11_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
